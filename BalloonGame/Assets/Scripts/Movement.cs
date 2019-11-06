562fff3bb5752e93e3f034ac1e531d7332916643
niemand322 <55998944+niemand322@users.noreply.github.com>
2019-11-06 20:27:11 +0100

    Player Movement
    Added a player gameobject.
    Added temporary sprite.
    Added player movement(-script).
    Changed camera settings to fit a 2d game.

---
+++ BalloonGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float horizontalSpeed = 10;
+    public float verticalSpeed = 3;
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * horizontalSpeed, vertical * verticalSpeed);
+    }
+}
