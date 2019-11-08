9f5aa9e9bda9fa526c049b3e8937cb60fc9ee139
niemand322 <55998944+niemand322@users.noreply.github.com>
2019-11-08 19:50:31 +0100

    Prototype of a lightswitch

---
+++ BalloonGame/Assets/Lightswitch/Lightswitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+
+    //Scale this to the size of your room
+    public GameObject darkness;
+
+    SpriteRenderer darknessSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        darknessSprite = darkness.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        darknessSprite.color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+        darkness.transform.position = new Vector3(1, 2, 7);
+    }
+}
