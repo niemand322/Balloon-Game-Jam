1addcda5d75e408c83e0e1d1e9606e9230a7f59e
Raoul Marais <raoulmarais@gmail.com>
2019-11-15 22:14:42 +0200

    Changes to the font syyle - using Chunk Five, and an attempt at a simple dialogue drawn frame.

---
+++ BalloonGame/Assets/Scripts/DeleteObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+
+    public GameObject picture;
+    public void DeleteGameObject(string tag)
+    {
+        Destroy(GameObject.FindGameObjectWithTag(tag));
+    }
+
+    public void ShowPicture(bool active)
+    {
+        picture.SetActive(active);
+    }
+}
---
+++ BalloonGame/Assets/Scripts/StartTintro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VIDE_Data;
+
+public class StartTintro : MonoBehaviour
+{
+    public UIManager uimanager;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if (!VD.isActive)
+            {
+                uimanager.Begin(GetComponent<VIDE_Assign>());
+            }
+        }
+    }
+}
