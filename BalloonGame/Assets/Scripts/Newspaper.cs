4897e3ed59591b1051bdbdcb2d6f3afb7f7c8e86
RayCoolsen <raycoolsen@gmx.de>
2019-11-18 19:34:20 +0100

    Newspaper ending

---
+++ BalloonGame/Assets/Scripts/Newspaper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newspaper : MonoBehaviour
+{
+    public GameObject newspaper;
+    public GameObject NPC_Container;
+    public GameObject Player_Container;
+
+    public void News(string news)
+    {
+        NPC_Container.SetActive(false);
+        Player_Container.SetActive(false);
+        Instantiate(newspaper, Vector3.zero, Quaternion.identity);
+    }
+}
